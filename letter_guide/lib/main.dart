98519706dd51e82fe4020d4ba0080d0d27b226cd
Furkan Asani <furkan@chrono24.com>
2023-04-13 09:01:20 +0200

    Use the route_constants in the main and the MainMenuButton also accepts a route now

--- letter_guide/lib/main.dart
+++ letter_guide/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:letter_guide/pages/document_summary_page.dart';
+import 'package:letter_guide/widgets/document_scan/document_scanner_widget.dart';
+import 'package:letter_guide/constants/route_constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +28,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'LetterGuide'),
+      routes: {
+        RouteConstants.documentScanner.name: (context) =>
+            const LetterGuideScaffold(body: DocumentScannerWidget(), title: 'Document Scanner',),
+        RouteConstants.documentSummary.name: (context) =>
+            const LetterGuideScaffold(body: DocumentSummaryPage(), title: 'Document Summary')
+      },
     );
   }
 }
@@ -49,7 +57,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -61,67 +68,76 @@ class _MyHomePageState extends State<MyHomePage> {
     return LetterGuideScaffold(
       title: 'LetterGuide',
       body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const <Widget>[
-              MainMenuButton(
-                  icon: Icon(Icons.camera_alt, color: Colors.white),
-                  buttonTitle: "Scan"),
-              MainMenuButton(
-                icon: Icon(
-                  Icons.history,
-                  color: Colors.white,
-                ),
-                buttonTitle: "Historie",
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            MainMenuButton(
+                icon: Icon(Icons.camera_alt, color: Colors.white),
+                buttonTitle: "Scan",
+            route: RouteConstants.documentScanner.name,),
+            MainMenuButton(
+              icon: Icon(
+                Icons.history,
+                color: Colors.white,
               ),
-              MainMenuButton(
-                icon: Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                ),
-                buttonTitle: "Einstellungen",
-              )
-            ],
-          ),
+              buttonTitle: "Historie",
+              route: RouteConstants.documentSummary.name,
+            ),
+            MainMenuButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              buttonTitle: "Einstellungen",
+              route: RouteConstants.documentSummary.name,
+            )
+          ],
         ),
+      ),
     );
   }
 }
 
-class LetterGuideScaffold extends StatelessWidget{
+class LetterGuideScaffold extends StatelessWidget {
   final Widget body;
   final String title;
 
-  const LetterGuideScaffold({super.key, required this.body, required this.title});
-
+  const LetterGuideScaffold(
+      {super.key, required this.body, required this.title});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(title),),body: body);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: body);
   }
 }
 
 class MainMenuButton extends StatelessWidget {
   final Icon icon;
   final String buttonTitle;
+  final String route;
 
-  const MainMenuButton({super.key, required this.icon, required this.buttonTitle});
+  const MainMenuButton(
+      {super.key, required this.icon, required this.buttonTitle, required this.route});
 
   @override
   Widget build(BuildContext context) {
@@ -131,9 +147,9 @@ class MainMenuButton extends StatelessWidget {
           padding: const EdgeInsets.only(bottom: 8.0),
           child: ElevatedButton(
             onPressed: () {
-             Navigator.push(context, MaterialPageRoute(builder: (context) => const LetterGuideScaffold(body: DocumentSummaryPage(), title: 'Document Summary'))); // Do something when button is pressed
-
-          },
+              Navigator.pushNamed(
+                  context, route); // Do something when button is pressed
+            },
             style: ButtonStyle(
               shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                 RoundedRectangleBorder(
